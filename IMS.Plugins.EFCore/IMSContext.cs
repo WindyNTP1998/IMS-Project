635551fd6968a70f99d9ca5f8286c9daa92d14c0
Nguyen Thanh Phong <WindyNTP1998@gmail.com>
2022-03-21 16:09:36 +0700

    File-Structure

---
+++ IMS.Plugins.EFCore/IMSContext.cs
@@ -0,0 +1,26 @@
+﻿using IMS.CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Plugins.EFCore
+{
+    public class IMSContext : DbContext
+    {
+        public IMSContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Inventory> Inventories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inventory>().HasData(
+                 new Inventory { InventoryId = 1, InventoryName = "Gas Engine", Price = 1000, Quantity = 1 },
+                 new Inventory { InventoryId = 2, InventoryName = "Body", Price = 400, Quantity = 1 },
+                 new Inventory { InventoryId = 3, InventoryName = "Wheel", Quantity = 4, Price = 100 },
+                 new Inventory { InventoryId = 4, InventoryName = "Seat", Price = 50, Quantity = 5 },
+                 new Inventory { InventoryId = 5, InventoryName = "Electric Engine", Price = 8000, Quantity = 2 },
+                 new Inventory { InventoryId = 6, InventoryName = "Battery", Price = 400, Quantity = 5 }
+             );
+        }
+    }
+}
---
+++ IMS.WebApp/Program.cs
@@ -0,0 +1,67 @@
+using IMS.Plugins.EFCore;
+using IMS.UseCases;
+using IMS.UseCases.PluginInterfaces;
+using IMS.WebApp.Areas.Identity;
+using IMS.WebApp.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddTransient<IInventoryRepository, InventoryRepository>();
+builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
+
+
+builder.Services.AddDbContext<IMSContext>(options =>
+{
+    options.UseInMemoryDatabase("IMS");
+});
+
+WebApplication? app = builder.Build();
+
+IServiceScope? scope = app.Services.CreateScope();
+IMSContext? imsContext = scope.ServiceProvider.GetRequiredService<IMSContext>();
+imsContext.Database.EnsureDeleted();
+imsContext.Database.EnsureCreated();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
