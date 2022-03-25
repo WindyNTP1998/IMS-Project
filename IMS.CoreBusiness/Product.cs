f4c9265f8c6f5048c9ad0acdc77a0ef696c7d520
WindyNTP1998 <WindyNTP1998@gmail.com>
2022-03-25 17:59:24 +0700

    Revert "first-commit"
    This reverts commit 3a05193f60f097420da83122e7ad705714043788.

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -11,6 +11,5 @@ namespace IMS.CoreBusiness
         public int Quantity { get; set; }
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater than 0")]
         public double Price { get; set; }
-        public List<ProductInventory>? ProductInventories { get; set; }
     }
 }
